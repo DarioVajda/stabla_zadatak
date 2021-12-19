2229affad21812b2f57249401d88c22a73dad13e
Zdenja70 <bogdanzdravkovic04@gmail.com>
2021-12-19 19:59:28 +0100

    mlacare

--- main.c
+++ main.c
@@ -4,6 +4,17 @@
 
 int main()
 {
+    node* root=NULL;
+    dodaj(root);
+    printf("Stablo koje ste uneli u preorder redosledu:\n");
+    print(root);
+    printf("\nBroj cvorova stabla je %d\n", broj_cvorova(root));
+    printf("Broj listova stabla je %d\n", broj_listova(root));
+    printf("Proizvod cvorova stabla je %d\n", proizvod_cvorova(root));
+    printf("Broj prostih brojeva je %d\n", broj_prostih(root));
+    printf("Najmanji element stabla je %d\n", najmanji(root));
+    printf("Prosecna vrednost cvorova u stablu je %.2f", prosek(root));
+    printf("Proizvod vrednosti manjih od korena je %d\n", proizvod_cvorova(root->left));
 
     return 0;
 }
