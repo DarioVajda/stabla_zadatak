f3b43bcc1603fb26f5d76313995191075e02603e
Dario Vajda <vajdadario@gmail.com>
2021-12-19 20:27:36 +0100

    gotovo je sve

--- stablo.h
+++ stablo.h
@@ -1,6 +1,22 @@
 #ifndef STABLO_H_INCLUDED
 #define STABLO_H_INCLUDED
 
+typedef struct Node
+{
+    int data;
+    struct Node* left;
+    struct Node* right;
+} node;
 
+int broj_cvorova(node* root);
+int broj_listova(node* root);
+int proizvod_cvorova(node* root);
+int broj_prostih(node* root);
+int najmanji(node* root);
+float prosek(node* root);
+int minimum(node* root);
+node* novi(int value);
+void dodaj(node** root, int value);
+void print(node* root);
 
 #endif // STABLO_H_INCLUDED
